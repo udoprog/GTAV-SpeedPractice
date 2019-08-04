4b086f72c4da99f238e89709e35911eac2a06e43
John-John Tedro <udoprog@tedro.se>
2019-08-04 14:54:23 +0200

    Adjust some waypoints

--- script.cpp
+++ script.cpp
@@ -1,7 +1,9 @@
 #include "script.h"
 #include "keyboard.h"
 
+#include <optional>
 #include <sstream>
+#include <fstream>
 #include <vector>
 
 class Color {
@@ -148,16 +150,32 @@ public:
 
 class CheckpointPosition {
 public:
-	CheckpointPosition() = default;
+	CheckpointPosition(
+		CheckpointKind Kind,
+		Vec3 Pos,
+		Vec3 PointTo
+	) : Kind(Kind), Pos(Pos), PointTo(PointTo), Radius(10) {
+	}
+
+	CheckpointPosition(
+		CheckpointKind Kind,
+		Vec3 Pos,
+		Vec3 PointTo,
+		float Radius
+	) : Kind(Kind), Pos(Pos), PointTo(PointTo), Radius(Radius) {
+	}
 
 	CheckpointConfig Config() {
+		float radius = this->Radius;
+		float offsetZ = 2 * (10 / radius);
+
 		switch (Kind) {
 		case Waypoint:
-			return { RingSingleArrow, { 255, 255, 255, 64 }, 10, { 0, 0, 2 }, White1 };
+			return { RingSingleArrow, { 255, 255, 255, 64 }, radius, { 0, 0, offsetZ }, White1 };
 		case PowerPoint:
-			return { RingSingleArrow, { 255, 255, 0, 64 }, 10, { 0, 0, 2 }, LightYellow1 };
+			return { RingSingleArrow, { 255, 255, 0, 64 }, radius, { 0, 0, offsetZ }, LightYellow1 };
 		case BreakBoost:
-			return { RingSingleArrow, { 255, 0, 0, 64 }, 10, { 0, 0, 2 }, Red };
+			return { RingSingleArrow, { 255, 0, 0, 64 }, radius, { 0, 0, offsetZ }, Red };
 		case End:
 			return { Cyclinder, { 255, 255, 255, 64 }, 10, { 0, 0, -2 }, LightYellow2 };
 		}
@@ -166,6 +184,7 @@ public:
 	CheckpointKind Kind;
 	Vec3 Pos;
 	Vec3 PointTo;
+	float Radius;
 };
 
 class Checkpoint {
@@ -173,6 +192,7 @@ public:
 	Checkpoint(CheckpointType type, Vec3 pos, Vec3 pointTo, float radius, Color color, Vec3 checkPos, BlipColor blipColor) {
 		Pos = pos;
 		CheckPos = checkPos;
+		Radius2 = radius * radius;
 		handle = GRAPHICS::CREATE_CHECKPOINT(type, pos.x, pos.y, pos.z, pointTo.x, pointTo.y, pointTo.z, radius, color.r, color.g, color.b, color.a, 0);
 		blip = UI::ADD_BLIP_FOR_RADIUS(pos.x, pos.y, pos.z, radius);
 		UI::SET_BLIP_COLOUR(blip, blipColor);
@@ -185,6 +205,7 @@ public:
 
 	Vec3 Pos;
 	Vec3 CheckPos;
+	float Radius2;
 
 private:
 	int handle;
@@ -236,6 +257,11 @@ public:
 	// Reset the mission.
 	Checkpoint* Reset() {
 		Current = 0;
+
+		std::ostringstream s;
+		s << "Training: ~b~" << Name << "~w~";
+		Notify(s.str().c_str());
+
 		return Advance();
 	}
 
@@ -258,6 +284,38 @@ void Notify(const char* s) {
 	UI::_SET_NOTIFICATION_TEXT_ENTRY("STRING");
 	UI::_ADD_TEXT_COMPONENT_STRING3((char*)s);
 	UI::_DRAW_NOTIFICATION(true, true);
+}
+
+void DebugPosition(Ped ped, Mission* current) {
+	auto coords = ENTITY::GET_ENTITY_COORDS(ped, true);
+	auto forward = ENTITY::GET_ENTITY_FORWARD_VECTOR(ped);
+
+	std::ostringstream stream;
+
+	stream << "Training: ~y~" << current->Name << "~w~";
+	stream << " current: " << current->Current << "\n";
+
+	stream
+		<< "xyz (" << coords.x << ", " << coords.y << ", " << coords.z << "), "
+		<< "-> (" << forward.x << ", " << forward.y << ", " << forward.z << ")";
+
+	std::string s = stream.str();
+	ShowSubtitle(s.c_str());
+}
+
+void DebugMissionEnd() {
+	std::string name = "?";
+
+	if (SCRIPT::HAS_SCRIPT_LOADED("franklin0")) {
+		name = "Franklin & Lamar";
+	} else if (SCRIPT::HAS_SCRIPT_LOADED("armenian2")) {
+		name = "Repossession";
+	}
+
+	std::ostringstream stream;
+	stream << "Mission: " << AUDIO::IS_MISSION_COMPLETE_PLAYING() << "flag: " << GAMEPLAY::GET_MISSION_FLAG() << " (" << name << ")";
+	std::string s = stream.str();
+	ShowSubtitle(s.c_str());
 }
 
 void ScriptMain() {
@@ -326,8 +384,20 @@ void ScriptMain() {
 
 		Vec3 coords = ENTITY::GET_ENTITY_COORDS(ped, true);
 
+		if (IsKeyJustUp(VK_DELETE, true)) {
+			if (current != nullptr) {
+				delete point;
+				point = current->Advance();
+
+				std::ofstream os;
+				os.open("coords.txt", std::ofstream::out | std::ofstream::app);
+
+				os << "{" << coords.x << ", " << coords.y << ", " << coords.z << "}" << std::endl;
+			}
+		}
+
 		if (point != nullptr) {
-			if (point->CheckPos.Vdist2(coords) < 20) {
+			if (point->CheckPos.Vdist2(coords) < point->Radius2) {
 				delete point;
 
 				if (current != nullptr) {
@@ -339,20 +409,7 @@ void ScriptMain() {
 		if (debug) {
 			if (ticker > 0.05) {
 				if (current != nullptr) {
-					auto coords = ENTITY::GET_ENTITY_COORDS(ped, true);
-					auto forward = ENTITY::GET_ENTITY_FORWARD_VECTOR(ped);
-
-					std::ostringstream stream;
-
-					stream << "Training: ~y~" << current->Name << "~w~";
-					stream << " current: " << current->Current << "\n";
-
-					stream
-						<< "xyz (" << coords.x << ", " << coords.y << ", " << coords.z << "), "
-						<< "-> (" << forward.x << ", " << forward.y << ", " << forward.z << ")";
-
-					std::string s = stream.str();
-					ShowSubtitle(s.c_str());
+					DebugPosition(ped, current);
 				}
 
 				ticker = 0;
@@ -366,70 +423,70 @@ void ScriptMain() {
 Mission* FranklinAndLamar() {
 	Mission *points = new Mission("Franklin & Lamar");
 
-	points->Push({ Waypoint, Vec3(-1888.21, -564.945, 11.244), Vec3(-2015.77, -455.14, 11.0581) });
+	points->Push({ Waypoint, {-1886.48, -561.961, 11.2672}, {-2015.77, -455.14, 11.0581} });
 
-	points->Push({ Waypoint, Vec3(-2015.77, -455.14, 11.0581), Vec3(-1994, -419, 12) });
+	points->Push({ Waypoint, {-2015.77, -455.14, 11.0581}, {-1994, -419, 12} });
 
-	points->Push({ Waypoint, Vec3(-1994, -419, 12), Vec3(-1955, -424, 17) });
-	points->Push({ Waypoint, Vec3(-1857, -487, 26), Vec3(-1813, -517, 30) });
+	points->Push({ Waypoint, {-1994, -419, 12}, {-1955, -424, 17} });
+	points->Push({ Waypoint, {-1857, -487, 26}, {-1813, -517, 30} });
 
 	// power point to prevent from going off the top of the hill.
-	points->Push({ PowerPoint, Vec3(-1755.92, -550.803, 36.2118), Vec3(-1733.77, -557.012, 36.7685) });
-	points->Push({ Waypoint, Vec3(-1712, -563, 36), Vec3(-1643, -597, 33) });
-	points->Push({ BreakBoost, Vec3(-1637, -596, 33), Vec3(-1635, -597, 33) });
-	points->Push({ PowerPoint, Vec3(-1535, -642, 28), Vec3(-1492, -631, 30) });
-	points->Push({ BreakBoost, Vec3(-1479, -618, 30), Vec3(-1474, -614.5, 30.5) });
+	points->Push({ PowerPoint, {-1755.92, -550.803, 36.2118}, {-1733.77, -557.012, 36.7685} });
+	points->Push({ Waypoint, {-1712, -563, 36}, {-1643, -597, 33} });
+	points->Push({ BreakBoost, {-1637, -596, 33}, {-1635, -597, 33} });
+	points->Push({ PowerPoint, {-1535, -642, 28}, {-1492, -631, 30} });
+	points->Push({ BreakBoost, {-1479, -618, 30}, {-1474, -614.5, 30.5} });
 
 	// intersection 1
-	points->Push({ Waypoint, Vec3(-1393, -569, 30), Vec3(-1363, -550, 30) });
+	points->Push({ Waypoint, {-1316.15, -513.217, 32.6707}, {-1363, -550, 30} });
 	// intersection 2
-	points->Push({ Waypoint, Vec3(-1241, -448, 33), Vec3(-1181, -420.5, 34) });
+	points->Push({ Waypoint, {-1241, -448, 33}, {-1181, -420.5, 34} });
 	// turn before studio
-	points->Push({ Waypoint, Vec3(-1099, -414, 36), Vec3(-1075, -434, 36) });
+	points->Push({ Waypoint, {-1099, -414, 36}, {-1075, -434, 36} });
 	// entrance to studio
-	points->Push({ Waypoint, Vec3(-1055.55, -469.124, 35.8914), Vec3(-1061.56, -483.848, 36.102) });
+	points->Push({ Waypoint, {-1055.55, -469.124, 35.8914}, {-1061.56, -483.848, 36.102} });
 	// inside studio
-	points->Push({ Waypoint, Vec3(-1113.38, -512.257, 34.2677), Vec3(-1133.38, -522.442, 32.8011) });
+	points->Push({ Waypoint, {-1113.38, -512.257, 34.2677}, {-1133.38, -522.442, 32.8011} });
 	// exit to studio
-	points->Push({ Waypoint, Vec3(-1207.22, -580.16, 26.6866), Vec3(-1198.44, -633.651, 23.7369) });
+	points->Push({ Waypoint, {-1207.22, -580.16, 26.6866}, {-1198.44, -633.651, 23.7369} });
 	// after studio
-	points->Push({ Waypoint, Vec3(-1177.18, -666.784, 22.3475), Vec3(-1143.39, -700.038, 20.895) });
+	points->Push({ Waypoint, {-1173.97, -666.67, 22.5446}, {-1143.39, -700.038, 20.895}, 30 });
 	// turn towards truck
-	points->Push({ Waypoint, Vec3(-1108.82, -761.182, 18.7327), Vec3(-1132.03, -813.194, 15.3551) });
+	points->Push({ Waypoint, {-1108.82, -761.182, 18.7327}, {-1132.03, -813.194, 15.3551} });
 	// turn down towards building site
-	points->Push({ Waypoint, Vec3(-1157.63, -849.399, 13.7151), Vec3(-1151.35, -876.797, 10.5292) });
+	points->Push({ Waypoint, {-1157.63, -849.399, 13.7151}, {-1151.35, -876.797, 10.5292} });
 	// first jump
-	points->Push({ Waypoint, Vec3(-1090.76, -979.072, 3.9196), Vec3(-1037.43, -1071.6, 3.66611) });
-	points->Push({ Waypoint, Vec3(-1037.43, -1071.6, 3.66611), Vec3(-979.515, -1162.63, 3.63968) });
-	points->Push({ PowerPoint, Vec3(-979.515, -1162.63, 3.63968), Vec3(-946.05, -1194.59, 4.64777) });
+	points->Push({ Waypoint, {-1090.76, -979.072, 3.9196}, {-1037.43, -1071.6, 3.66611} });
+	points->Push({ Waypoint, {-1037.43, -1071.6, 3.66611}, {-979.515, -1162.63, 3.63968} });
+	points->Push({ PowerPoint, {-979.515, -1162.63, 3.63968}, {-946.05, -1194.59, 4.64777} });
 	// turn up towards vespucci road.
-	points->Push({ PowerPoint, Vec3(-946.381, -1197.46, 4.65006), Vec3(-907.4, -1188.37, 4.62585) });
+	points->Push({ PowerPoint, {-946.381, -1197.46, 4.65006}, {-907.4, -1188.37, 4.62585} });
 	// intersection
-	points->Push({ Waypoint, Vec3(-797.602, -1122.58, 10.2477), Vec3(-762.629, -1104.09, 10.3714) });
+	points->Push({ Waypoint, {-797.602, -1122.58, 10.2477}, {-762.629, -1104.09, 10.3714} });
 	// hill up
-	points->Push({ Waypoint, Vec3(-679.172, -1049.98, 15.7689), Vec3(-659.784, -1026.22, 17.745) });
+	points->Push({ Waypoint, {-679.172, -1049.98, 15.7689}, {-659.784, -1026.22, 17.745} });
 	// turn towards highway
-	points->Push({ PowerPoint, Vec3(-619.136, -877.421, 24.6516), Vec3(-597.697, -856.391, 25.2748) });
+	points->Push({ PowerPoint, {-619.136, -877.421, 24.6516}, {-597.697, -856.391, 25.2748} });
 
-	points->Push({ Waypoint, Vec3(-519.747, -843.033, 30.0213), Vec3(-482.834, -843.755, 30.0885) });
+	points->Push({ Waypoint, {-518.104, -837.862, 30.0275}, {-482.834, -843.755, 30.0885}, 30 });
 
-	points->Push({ PowerPoint, Vec3(-295.956, -835.602, 31.3429), Vec3(-266.187, -797.451, 31.7652) });
+	points->Push({ PowerPoint, {-295.956, -835.602, 31.3429}, {-266.187, -797.451, 31.7652} });
 
-	points->Push({ Waypoint, Vec3(-222.812, -695.03, 33.1329), Vec3(-183.073, -679.134, 33.8081) });
-	points->Push({ Waypoint, Vec3(-86.6128, -677.583, 34.8648), Vec3(-75.0701, -681.3, 33.5304) });
+	points->Push({ Waypoint, {-222.812, -695.03, 33.1329}, {-183.073, -679.134, 33.8081} });
+	points->Push({ Waypoint, {-86.6128, -677.583, 34.8648}, {-75.0701, -681.3, 33.5304} });
 
-	points->Push({ PowerPoint, Vec3(63.1746, -625.133, 31.2793), Vec3(-54.6064, -756, 31.257) });
-	points->Push({ Waypoint, Vec3(54.6064, -756.0, 31.257), Vec3(135.0, -808.832, 30.8428) });
+	points->Push({ PowerPoint, {63.1746, -625.133, 31.2793}, {-54.6064, -756, 31.257} });
+	points->Push({ Waypoint, {54.6064, -756.0, 31.257}, {135.0, -808.832, 30.8428} });
 
-	points->Push({ Waypoint, Vec3(155.112, -831.111, 30.6528), Vec3(108.104, -979.07, 28.9833) });
+	points->Push({ Waypoint, {155.112, -831.111, 30.6528}, {108.104, -979.07, 28.9833} });
 
-	points->Push({ Waypoint, Vec3(108.104, -979.07, 28.9833), Vec3(71.3636, -1053.01, 28.9159) });
+	points->Push({ Waypoint, {108.104, -979.07, 28.9833}, {71.3636, -1053.01, 28.9159} });
 	// pointing into the alley.
-	points->Push({ Waypoint, Vec3(71.3636, -1053.01, 28.9159), Vec3(-32.0474, -1020.51, 28.439) });
+	points->Push({ Waypoint, {71.3636, -1053.01, 28.9159}, {-32.0474, -1020.51, 28.439} });
 	// down the alley with the cat.
-	points->Push({ Waypoint, Vec3(-32.0474, -1020.51, 28.439), Vec3(-51.1589, -1076.17, 26.518) });
+	points->Push({ Waypoint, {-32.0474, -1020.51, 28.439}, {-51.1589, -1076.17, 26.518} });
 
 	// Simeon's
-	points->Push({ End, Vec3(-51.1589, -1076.17, 26.518), Vec3(0, 0, 0) });
+	points->Push({ End, {-51.1589, -1076.17, 26.518}, {0, 0, 0} });
 	return points;
 }
--- script.h
+++ script.h
@@ -11,3 +11,9 @@ class Mission;
 Mission* FranklinAndLamar();
 
 void ScriptMain();
+
+// Show a subtitle with the given content.
+void ShowSubtitle(const char* s);
+
+// Draw a notification with the given content.
+void Notify(const char* s);
